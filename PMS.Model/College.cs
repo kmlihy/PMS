4b85871d30f4fb00f5892ea08de9a23b366c7f5f
PatrickStar <PatrickStar@DESKTOP-8HHB046>
2018-06-12 21:25:08 +0800

    添加选题批次和学院实体类

---
+++ PMS.Model/College.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMS.Model
+{
+    /// <summary>
+    /// 分院
+    /// </summary>
+    public class College
+    {
+        /// <summary>
+        /// 分院编号
+        /// </summary>
+        public int ColID { get; set; }
+        /// <summary>
+        /// 分院名称
+        /// </summary>
+        public string ColName { get; set; }
+
+        public College() { }
+    }
+}
---
+++ PMS.Model/Plan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMS.Model
+{
+    /// <summary>
+    /// 选题批次
+    /// </summary>
+    public class Plan
+    {
+        /// <summary>
+        /// 选题批次编号
+        /// </summary>
+        public int PlanId { get; set; }
+        /// <summary>
+        /// 选题批次名
+        /// </summary>
+        public string PlanName { get; set; }
+        /// <summary>
+        /// 选题开始时间
+        /// </summary>
+        public DateTime StartTime { get; set; }
+        /// <summary>
+        /// 选题截止时间
+        /// </summary>
+        public DateTime EndTime { get; set; }
+        /// <summary>
+        /// 批次状态0代表未激活，1代表激活
+        /// </summary>
+        public int State { get; set; }
+        /// <summary>
+        /// 批次所属分院
+        /// </summary>
+        public College college { get; set; }
+
+        public Plan() { }
+    }
+}
