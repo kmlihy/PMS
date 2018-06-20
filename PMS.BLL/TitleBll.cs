25fd62ea39893b2a505d51b226c2c82865f9fdb7
潘江涛 <1206631156@qq.com>
2018-06-20 19:09:42 +0800

    实现题目和公告的增删改查的编写，并测试通过

---
+++ PMS.BLL/TitleBll.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PMS.Dao;
+using PMS.Model;
+
+namespace PMS.BLL
+{
+    using System.Data;
+    using Result = Enums.OpResult;
+    public class TitleBll
+    {
+        private TitleDao dao = new TitleDao();
+
+        /// <summary>
+        /// 添加题目
+        /// </summary>
+        /// <param name="news">题目实体</param>
+        /// <returns>返回处理结果</returns>
+        public Enums.OpResult Insert(Title title)
+        {
+            int count = dao.Insert(title);
+            if (count>0)
+            {
+                return Result.添加成功;
+            }
+            else
+            {
+                return Result.添加失败;
+            }
+        }
+
+        /// <summary>
+        /// 修改题目
+        /// </summary>
+        /// <param name="news">题目实体</param>
+        /// <returns>返回处理结果</returns>
+        public Enums.OpResult Delete(int titleId)
+        {
+            int count = dao.Delete(titleId);
+            if (count>0)
+            {
+                return Result.删除成功;
+            }
+            else
+            {
+                return Result.删除失败;
+            }
+        }
+
+        public Enums.OpResult Update(Title title)
+        {
+            int count = dao.Update(title);
+            if (count>0)
+            {
+                return Result.更新成功;
+            }
+            else
+            {
+                return Result.更新失败;
+            }
+        }
+
+        /// <summary>
+        /// 根据id查询题目信息
+        /// </summary>
+        /// <param name="newsId">题目id</param>
+        /// <returns>返回查询结果</returns>
+        public DataSet Select(int titleId)
+        {
+            DataSet ds = dao.Select(titleId);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 根据题目的名称和所属专业名称进行模糊查询
+        /// </summary>
+        /// <param name="likeStrwhere">查询条件</param>
+        /// <returns>返回查询结果</returns>
+        public DataSet Select(string likeStrwhere)
+        {
+            DataSet ds = dao.Select(likeStrwhere);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                return ds;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 获得题目实体对象
+        /// </summary>
+        /// <param name="titleId"></param>
+        /// <returns></returns>
+        public Title GetTitle(int titleId)
+        {
+           return dao.GetTitle(titleId);
+        }
+
+
+    }
+}
---
+++ PMS.Dao/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using PMS.DBHelper;
+
+namespace PMS.Dao
+{
+    class News
+    {
+        SQLHelper db = new SQLHelper();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        public DataSet select(String search)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select * from T_News");
+            strSql.Append("where @search");
+            String[] param = { "@search" };
+            String[] value = { search };
+            return db.FillDataSet(strSql.ToString(),param,value);
+        }
+    }
+}
