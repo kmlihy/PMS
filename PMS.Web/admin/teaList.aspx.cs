aeebd521f137f7810e826706a37ba7d72b8f1245
GYH <GYH@DESKTOP-LN7K982>
2018-06-21 20:54:39 +0800

    提交选题记录表的实体修改

--- PMS.Web/admin/teaList.aspx.cs
+++ PMS.Web/admin/teaList.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PMS.BLL;
+using PMS.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,30 @@ namespace PMS.Web.admin
 {
     public partial class teaList : System.Web.UI.Page
     {
+        protected DataSet ds=null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            getdata("", 1);
+        }
 
+        public void getdata(String strWhere, int IntPageNum)
+        {
+            BLL.TeacherBll sdao = new TeacherBll();
+            Dao.PublicProcedure pro = new Dao.PublicProcedure();
+            TableBuilder tabuilder = new TableBuilder()
+            {
+                StrTable = "V_Teacher",
+                StrWhere = strWhere,
+                IntColType = 0,
+                IntOrder = 0,
+                IntPageNum = IntPageNum,
+                IntPageSize = 2,
+                StrColumn = "teaAccount",
+                StrColumnlist = "*"
+            };
+            int count;
+            ds = pro.SelectBypage(tabuilder, out count);
         }
     }
 }
