33532a5e1f93ecf046006339e0d36660e333b064
Administrator <Administrator@ZHDN-20180316PL>
2018-06-21 20:58:00 +0800

    修改教师类型信息0超级管理员，1教师，2分院管理员

---
+++ PMS.Dao/TitleRecordDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMS.Dao
+{
+    /// <summary>
+    /// 选题记录
+    /// </summary>
+    public class TitleRecordDao
+    {
+    }
+}
