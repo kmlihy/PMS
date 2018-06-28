0af002e2e00b7d4b7765ec553c21ac7dfcf2db4c
Administrator <Administrator@SC-201805141210>
2018-06-28 16:31:38 +0800

    选定题目页面数据绑定提交

--- PMS.Web/PaperDtailStu.aspx.cs
+++ PMS.Web/PaperDtailStu.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using PMS.BLL;
+using PMS.Model;
 
 namespace PMS.Web
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        protected Title titleId = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            TitleBll titleBll = new TitleBll();
+            string tId = Request.QueryString["titleId"];
+            titleId = titleBll.GetTitle(int.Parse(tId));
         }
     }
 }
