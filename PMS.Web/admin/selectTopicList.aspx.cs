837282776c8bbb7e93c1c0400963d21f3b65dfae
jock <jock@DESKTOP-I2S4DFK>
2018-07-04 17:17:59 +0800

    批量导入模态框

--- PMS.Web/admin/selectTopicList.aspx.cs
+++ PMS.Web/admin/selectTopicList.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.WebControls;
 
 namespace PMS.Web.admin
 {
+    using Result = Enums.OpResult;
     public partial class selectTopicList : System.Web.UI.Page
     {
         //列表
@@ -17,6 +18,8 @@ namespace PMS.Web.admin
         //专业
         protected DataSet prods = null;
         protected ProfessionBll probll = new ProfessionBll();
+
+        TitleRecordBll titrecordbll = new TitleRecordBll();
         //学院
         protected DataSet bads = null;
         protected CollegeBll colbll = new CollegeBll();
@@ -27,19 +30,73 @@ namespace PMS.Web.admin
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Search();
-            getPage(Search());
-            bads = colbll.Select();
-            prods = probll.Select();
-        }
+            
+            string op = Context.Request.Form["op"];
+            if (op=="del")
+            {
+                IsdeleteCollege();
+                delPro();
 
+
+            }
+            if (!IsPostBack) {
+                Search();
+                getPage(Search());
+                bads = colbll.Select();
+                prods = probll.Select();
+            }
+        }
+        //判断是否能删除
+        public Result IsdeleteCollege()
+        {
+            string recordid = Context.Request["Recordid"].ToString();
+            Result row = Result.记录不存在;
+            if (titrecordbll.IsDelete("T_Student", "proId", recordid) == Result.关联引用)
+            {
+                row = Result.关联引用;
+            }
+            if (titrecordbll.IsDelete("T_Title", "proId", recordid) == Result.关联引用)
+            {
+                row = Result.关联引用;
+            }
+            return row;
+        }
+        //删除
+        public void delPro()
+        {
+            int recordid = int.Parse(Context.Request["Recordid"].ToString());
+            Result row = IsdeleteCollege();
+            if (row == Result.记录不存在)
+            {
+                Result result = titrecordbll.delete(recordid);
+
+                if (result == Result.删除成功)
+                {
+                    Response.Write("删除成功");
+                    Response.End();
+                }
+                else
+                {
+                    Response.Write("删除失败");
+                    Response.End();
+                }
+            }
+            else
+            {
+                Response.Write("在其他表中有关联不能删除");
+                Response.End();
+            }
+        }
+        //获取数据
         public void getPage(String strWhere)
         {
-            string currentPage = Request.QueryString["currentPage"];
-            if (currentPage == null || currentPage.Length <= 0)
-            {
-                currentPage = "1";
-            }
+          
+             string currentPage = Request.QueryString["currentPage"];
+                if (currentPage == null || currentPage.Length <= 0)
+                {
+                    currentPage = "1";
+                }
+         
 
             TitleRecordBll titlerd = new TitleRecordBll();
             TableBuilder tabuilder = new TableBuilder()
@@ -56,6 +113,7 @@ namespace PMS.Web.admin
             getCurrentPage = int.Parse(currentPage);
             ds = titlerd.SelectBypage(tabuilder, out count);
         }
+        //搜索
         public string Search()
         {
             try
