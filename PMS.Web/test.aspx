4b4c8f6e95c89d93c2bd6b8d50314d7be50d792a
xzl <1634602832@qq.com>
2018-07-09 11:17:30 +0800

    Excel导入test实现

--- PMS.Web/test.aspx
+++ PMS.Web/test.aspx
@@ -10,42 +10,63 @@
         <script src="js/jquery-3.3.1.min.js"></script>
         <script src="js/ajaxfileupload.js"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <style>
+		       .file {
+                position: relative;
+                display: inline-block;
+                background: #D0EEFF;
+                border: 1px solid #99D3F5;
+                border-radius: 4px;
+                padding: 4px 12px;
+                overflow: hidden;
+                color: #1E88C7;
+                text-decoration: none;
+                text-indent: 0;
+                line-height: 20px;
+               }
+                .file input {
+                    position: absolute;
+                    font-size: 100px;
+                    right: 0;
+                    top: 0;
+                    opacity: 0;
+                }
+                .file:hover {
+                    background: #AADFFD;
+                    border-color: #78C3F3;
+                    color: #004974;
+                    text-decoration: none;
+                }
+        </style>
     </head>
     <body>
         <div class="container kv-main">
-            <form enctype="multipart/form-data">
-               <button type="button" class="btn btn-success" id="btnupload">上传</button>
-           	    <!-- 隐藏file标签 -->  
-		        <input id="fileToUpload" style="display:none" type="file" name="upfile" />        
+            <form id="form1" runat="server" method="post" enctype="multipart/form-data" action="test.aspx?op=upload">
+                <div>
+                    <a href="javascript:;" class="file">选择文件
+                     <input type="file" name="upload" id="upload">
+                     <label class="showFileName"></label>
+                     <label class="fileerrorTip"></label>
+                    </a>
+                </div>
+                <button type="submit" class="btn btn-success" id="btnupload">上传</button>
             </form>
         </div>
     </body>
 	<script type="text/javascript">  
-	$(function(){
-		$("input") //将html控件实现jqury对象化  
-		$("#btnupload").click(function(){ 
-			$("#fileToUpload").click();
-		});
-	    //选择文件之后执行上传  
-        $("#fileToUpload").change(function () {
-            var path = $("#fileToUpload").val();
-            $.ajax({
-                type: "post",
-                url: "test.aspx?op=upload",
-                data: {
-                    Path: path,
-                },
-                datatype: 'text',
-                success: function (data) {
-                    alert(data);
-                },
-                error: function () {
-
-                }
-            })
-	    });  
-	      
-	});  
+        $(".file").on("change","input[type='file']",function(){
+            var filePath=$(this).val();
+            if(filePath.indexOf("xls")!=-1 || filePath.indexOf("xlsx")!=-1){
+                $(".fileerrorTip").html("").hide();
+                var arr=filePath.split('\\');
+                var fileName=arr[arr.length-1];
+                $(".showFileName").html(fileName);
+            }else{
+                $(".showFileName").html("");
+                $(".fileerrorTip").html("您未上传文件，或者您上传文件类型有误！").show();
+                return false 
+            }
+        })
 	</script>
 
 </html>
--- PMS.Web/test.aspx.cs
+++ PMS.Web/test.aspx.cs
@@ -14,73 +14,100 @@ namespace PMS.Web
     using Result = Enums.OpResult;
     public partial class test : System.Web.UI.Page
     {
-        protected string getName = "";
-        protected int getCurrentPage = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
             string op = Context.Request["op"];
-            string path = Context.Request["Path"];
-            if (op == "upload")
+            if (!IsPostBack)
             {
-                try
+
+            }
+            else
+            {
+                if (op == "upload")
                 {
-                    //"C:\\Users\\潘江涛\\Desktop\\新建 XLS 工作表.xls"
-                   
-                    //Teacher user =(Teacher)Session["user"];//取得登录用户用账号作为文件夹名称
-                    string x = path.Replace('\\', '*');
-                    string[] path1 = x.Split('*');
-                    string fileName = "";
-                    fileName = path1[path1.Length - 1];
-                    System.Diagnostics.Debug.WriteLine(fileName);
-                    
-                    string eddstring = path.Substring(path.Length - 4);//获取后四位后缀进行判断
-                    if (eddstring == ".xls" || eddstring == "xlsx")
+                    upload();
+                }
+            }
+
+
+        }
+
+        public void upload()
+        {
+            try
+            {
+                Teacher user = (Teacher)Session["user"];//取得登录用户用账号作为文件夹名称
+                HttpFileCollection file = HttpContext.Current.Request.Files;//从HTTP文件流中读取上传文件
+                if (file.Count > 0)
+                {
+                    //文件大小   
+                    long size = file[0].ContentLength;
+                    //文件类型   
+                    string type = file[0].ContentType;
+                    //文件名  IE浏览器文件名是绝对路径，服务器文件夹名不支持（\\），其他浏览器为文件名(兼容ie) 
+                    string filename = "";
+                    filename = file[0].FileName;
+                    if (filename.IndexOf("\\") != -1)//判断路径中是否包含\\
+                    {
+                        string[] a = filename.Split('\\');//分割字符串
+                        filename = a[a.Length - 1].ToString();//取数组最后一位作为文件夹名称
+                    }
+                    else
+                    {
+                        filename = file[0].FileName;//不是ie直接返回文件名称作为文件夹名
+                    }
+                    //文件格式   
+                    string tp = System.IO.Path.GetExtension(filename);
+
+                    if (tp == ".xls" || tp == "xlsx")
                     {
                         DirectoryInfo dir;
                         //将文件导入服务器
-                        string savePath = Server.MapPath("~/upload/学院信息导入Excel文件存储");//指定上传文件在服务器上的保存路 //获取服务器根目录
-                        dir= new DirectoryInfo(savePath);
+                        string savePath = Server.MapPath("~/upload/学院信息导入Excel文件存储");//指定上传文件在服务器上的保存路 
+                        dir = new DirectoryInfo(savePath);
                         dir.Create();
 
-                        savePath = Server.MapPath("~/upload/学院信息导入Excel文件存储/");
+
+                        savePath = Server.MapPath("~/upload/学院信息导入Excel文件存储/" + user.TeaAccount + "");
                         dir = new DirectoryInfo(savePath);
                         dir.Create();
 
                         DateTime d = DateTime.Now;
                         string datetime = d.ToString("yyyyMMddHHmmss");
 
-                        string name = datetime + "-"+fileName;//将当前时间作为文件名称
+                        string name = datetime + "-" + filename;//将当前时间作为文件名称
                         savePath = savePath + "\\" + name;//将路径合并
 
-                        DataTable data = ExcelHelper.GetDataTable(path);//从上传的目录里读取数据
-
-                        ExcelHelper.x2003.TableToExcelForXLS(data, savePath);//将读出的数据给定路径存入到服务器中
+                        file[0].SaveAs(savePath);//存入服务器
 
                         var dt = ExcelHelper.GetDataTable(savePath);//从服务器路径读取数据成DataTable
-
-                        //CollegeBll bll = new CollegeBll();
-                        ProfessionBll bll = new ProfessionBll();
-                        Result row = bll.upload(dt);
-                        if (row == Result.添加失败)
+                        CollegeBll bll = new CollegeBll();
+                        int row = bll.upload(dt);
+                        if (row > 0)
                         {
-                            Response.Write("导入失败");
+                            Page.ClientScript.RegisterClientScriptBlock(GetType(), "js", "<script>alert('导入失败');</script>");
                         }
                         else
                         {
-                            Response.Write("导入成功");
+                            Page.ClientScript.RegisterClientScriptBlock(GetType(), "js", "<script>alert('导入成功');</script>");
                         }
                     }
                     else
                     {
-                        this.Response.Write("Excel格式不正确");
+                        Page.ClientScript.RegisterClientScriptBlock(GetType(), "js", "<script>alert('Excel格式不正确');</script>");
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    this.Response.Write(ex.Message);
+                    Page.ClientScript.RegisterClientScriptBlock(GetType(), "js", "<script>alert('请选择上传文件');</script>");
                 }
             }
-
+            catch (Exception ex)
+            {
+                this.Response.Write(ex.Message);
+            }
         }
     }
 }
