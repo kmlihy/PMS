0824e9a5ab00ae8ffb2f73c280211e15369b2458
xzl <1634602832@qq.com>
2018-07-10 11:56:53 +0800

    权限管理添加

--- PMS.Web/admin/login.aspx.cs
+++ PMS.Web/admin/login.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using PMS.BLL;
+using System.Web.Security;
 
 namespace PMS.Web.admin
 {
@@ -17,6 +18,7 @@ namespace PMS.Web.admin
                 teaAccount = Request.Form["userName"].ToString();
                 string pwd = Request.Form["pwd"].ToString();
                 TeacherBll bll = new TeacherBll();
+                string roles = "administrator";
                 Model.Teacher teacher = bll.Login(teaAccount, pwd);
                 if (teacher!=null)
                 {
@@ -24,12 +26,20 @@ namespace PMS.Web.admin
                     {
                         Session["user"] = teacher;
                         Session["state"] = 0;
+                        FormsAuthenticationTicket Ticket = new FormsAuthenticationTicket(1, teaAccount, DateTime.Now, DateTime.Now.AddMinutes(30), true, roles); //建立身份验证票对象 
+                        string HashTicket = FormsAuthentication.Encrypt(Ticket); //加密序列化验证票为字符串 
+                        HttpCookie UserCookie = new HttpCookie(FormsAuthentication.FormsCookieName, HashTicket); //生成Cookie 
+                        Context.Response.Cookies.Add(UserCookie); //票据写入Cookie 
                         Response.Redirect("main.aspx");
                     }
                     else
                     {
                         Session["user"] = teacher;
-                        Session["state"] = 2;
+                        Session["state"] = 2;                       
+                        FormsAuthenticationTicket Ticket = new FormsAuthenticationTicket(1, teaAccount, DateTime.Now, DateTime.Now.AddMinutes(30), true, roles); //建立身份验证票对象 
+                        string HashTicket = FormsAuthentication.Encrypt(Ticket); //加密序列化验证票为字符串 
+                        HttpCookie UserCookie = new HttpCookie(FormsAuthentication.FormsCookieName, HashTicket); //生成Cookie 
+                        Context.Response.Cookies.Add(UserCookie); //票据写入Cookie 
                         Response.Redirect("main.aspx");
                     }                   
                 }               
