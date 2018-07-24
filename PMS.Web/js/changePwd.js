3c68585ad6870b15a34d4296f4d6c08756e7502e
Administrator <Administrator@SC-201805141210>
2018-07-24 13:35:00 +0800

    修改密码页面更改

---
+++ PMS.Web/js/changePwd.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    var oldPwd = $("#quondampwd").val(),
+        confirmPwd = $("#confirmPwd").val(),
+        newPwd = $("#newpwd").val();
+    //提交密码
+    $("#postPwd").click(function () {
+        if (oldPwd == confirmPwd) {
+            $.ajax({
+                url: "changePwd.aspx",
+                data: {
+                    oldPwd: oldPwd,
+                    newPwd: newPwd,
+                    op: "password"
+                },
+                type: 'Post',
+                dataType: 'text',
+                success: function (succ) {
+                    if (succ == "更新成功") {
+                        window.wxc.xcConfirm(succ, window.wxc.xcConfirm.typeEnum.success, {
+                            onOk: function (v) {
+                                jump(1);
+                            }
+                        });
+                    } else {
+                        window.wxc.xcConfirm(succ, window.wxc.xcConfirm.typeEnum.error, {
+                            onOk: function (v) {
+                                jump(1);
+                            }
+                        });
+                    }
+                }
+            })
+        }
+        else {
+            alert("两次输入的密码不一样！");
+        }
+    })
+    function jump(change) {
+        window.location.href = "changePwd.aspx?oldPwd=" + oldPwd + "&newPwd=" + newPwd;
+    }
+})
