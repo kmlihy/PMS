b706a65bff2cc1711900f7c799f9da3df6940473
Administrator <Administrator@SC-201805141210>
2018-07-27 11:20:15 +0800

    批次表查询按钮问题更改

--- PMS.Web/js/batchList.js
+++ PMS.Web/js/batchList.js
@@ -53,9 +53,14 @@ $(document).ready(function () {
     //点击按钮查询
     $("#btn-search").click(function () {
         var strWhere = $("#inputsearch").val();
-        sessionStorage.setItem("strWhere", strWhere);
-        sessionStorage.setItem("type", "btn");
-        jump(1);
+        if (strWhere != null || strWhere != "") {
+            sessionStorage.setItem("strWhere", strWhere);
+            sessionStorage.setItem("type", "btn");
+            jump(1);
+        }
+        else {
+            jump(1);
+        }
     });
     //下拉框查询
     $("#chooseStuPro").change(function () {
