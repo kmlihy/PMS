3945ac6003bec740c98b56dad94ad29c088a9091
周文慧 <1305419069@qq.com>
2018-09-07 19:00:46 +0800

    添加公告修改

--- PMS.Web/admin/addNews.aspx.cs
+++ PMS.Web/admin/addNews.aspx.cs
@@ -20,23 +20,48 @@ namespace PMS.Web.admin
                 string newsTitle = Request["newsTitle"].ToString();
                 string content = Request["content"].ToString();
                 //TODO将从登录的Session中取到公告发布对象
-                Teacher teacher = (Teacher)Session["user"];
-                NewsBll bll = new NewsBll();
-                News news = new News();
-                news.NewsTitle = newsTitle;
-                news.NewsContent = HttpUtility.UrlDecode(content);
-                news.teacher = teacher;
-                news.CreateTime = DateTime.Now;
-                Result result = bll.Insert(news);
-                if (result == Result.添加成功)
+                int state = Convert.ToInt32(Session["state"]);
+                if (state == 0 || state == 2)
                 {
-                    Response.Write("添加成功");
-                    Response.End();
+                    Teacher teacher = (Teacher)Session["user"];
+                    NewsBll bll = new NewsBll();
+                    News news = new News();
+                    news.NewsTitle = newsTitle;
+                    news.NewsContent = HttpUtility.UrlDecode(content);
+                    news.teacher = teacher;
+                    news.CreateTime = DateTime.Now;
+                    Result result = bll.Insert(news);
+                    if (result == Result.添加成功)
+                    {
+                        Response.Write("添加成功");
+                        Response.End();
+                    }
+                    else
+                    {
+                        Response.Write("添加失败");
+                        Response.End();
+                    }
                 }
                 else
                 {
-                    Response.Write("添加失败");
-                    Response.End();
+                    Teacher teacher = (Teacher)Session["loginuser"];
+                    NewsBll bll = new NewsBll();
+                    News news = new News();
+                    news.NewsTitle = newsTitle;
+                    news.NewsContent = HttpUtility.UrlDecode(content);
+                    news.teacher = teacher;
+                    news.CreateTime = DateTime.Now;
+                    Result result = bll.Insert(news);
+                    if (result == Result.添加成功)
+                    {
+                        Response.Write("添加成功");
+                        Response.End();
+                    }
+                    else
+                    {
+                        Response.Write("添加失败");
+                        Response.End();
+                    }
                 }
             }
       
